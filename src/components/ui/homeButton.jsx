35189101a4878bc7adc998e3ff0ed244b340ca6a
Victor <lahr.victor@gmail.com>
2023-12-19 02:17:44 -0300

    fix: adjust header home button to be properly displayed on google chrome

--- src/components/ui/homeButton.jsx
+++ src/components/ui/homeButton.jsx
@@ -4,11 +4,11 @@ import { GoHomeFill } from 'react-icons/go';
 export default function HomeButton() {
   return (
     <Link href="/">
-      <button type="button" className="flex flex-row gap-1">
+      <button type="button" className="flex flex-row items-center justify-center gap-1">
         <div className="flex items-center justify-center h-10 p-1 text-3xl text-black bg-white rounded-md w-fit">
           <GoHomeFill />
         </div>
-        <p className="flex items-center justify-center text-sm font-bold">
+        <p className="flex text-sm font-bold">
           HOME
         </p>
       </button>
